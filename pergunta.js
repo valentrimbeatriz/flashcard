2d4a750450f02b1e909858236af2f1dbb728ac1c
valentrimbeatriz <valentim.beatriz@escola.pr.gov.br>
2025-07-03 16:40:28 -0300

    Update pergunta.js

--- pergunta.js
+++ pergunta.js
@@ -1,17 +1,52 @@
 criaCartao(
     'Lingua Portuguesa',
     'O que uma oracao sempre precisa ter para ser uma oracao?',
-    resposta
+    Uma oracao sempre precisa ter um verbo
 )
 
 criaCartao(
-    categoria,
+    Lingua Portuguesa,
+    O que indica quem faz ou sofre a ação do verbo em uma oracao,
+    O sujeito
+)
+
+criaCartao(
+    Lingua Portuguesa,
+    Qual parte da oração completa o sentido do verbo, mostrando o que acontece ou o resultado da ação?,
+    O predicado
+)
+criaCartao(
+    Lingua Portuguesa,
+    Como chamamos a oração que tem apenas um verbo ou locução verbal?
+    Oração simples
+)
+criaCartao(
+    Lingua Portuguesa,
     pergunta,
     resposta
 )
-
 criaCartao(
-    categoria,
+    Lingua Portuguesa,
+    pergunta,
+    resposta
+)
+criaCartao(
+    Lingua Portuguesa,
+    pergunta,
+    resposta
+)
+criaCartao(
+    Lingua Portuguesa,
+    pergunta,
+    resposta
+)
+criaCartao(
+    Lingua Portuguesa,
+    pergunta,
+    resposta
+)
+criaCartao(
+    Lingua Portuguesa,
     pergunta,
     resposta
 )
