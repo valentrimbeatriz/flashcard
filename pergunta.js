5d4e19c6f80b9626992f9d188d5831c9f3b16990
valentrimbeatriz <valentim.beatriz@escola.pr.gov.br>
2025-07-03 19:10:38 -0300

    Update pergunta.js

--- pergunta.js
+++ pergunta.js
@@ -9,44 +9,42 @@ criaCartao(
     O que indica quem faz ou sofre a ação do verbo em uma oracao,
     O sujeito
 )
-
 criaCartao(
-    Lingua Portuguesa,
-    Qual parte da oração completa o sentido do verbo, mostrando o que acontece ou o resultado da ação?,
-    O predicado
+   ' Lingua Portuguesa,'
+    'Qual parte da oracao completa o sentido do verbo, mostrando o que acontece ou o resultado da acao?',
+   ' O predicado'
 )
 criaCartao(
-    Lingua Portuguesa,
-    Como chamamos a oração que tem apenas um verbo ou locução verbal?
-    Oração simples
+  '  Lingua Portuguesa',
+    'Como chamamos a oracao que tem apenas um verbo ou locucao verbal?'
+    'Oracao Simples'
+criaCartao(
+   ' Lingua Portuguesa',
+  'Quem e o verbo da frase? Julio comprou banana',
+    'O verbo e comprou' 
 )
 criaCartao(
-    Lingua Portuguesa,
-    pergunta,
-    resposta
+    'Lingua Portuguesa',
+     'Se uma oração tem mais de um verbo ou locução verbal, como a chamamos?'
+     'Oração composta.'
 )
 criaCartao(
-    Lingua Portuguesa,
-    pergunta,
-    resposta
+   ' Lingua Portuguesa',
+   ' Quem é o sujeito da frase? "Maria comprou um remédio para sua irmã"'
+     'O sujeito é Maria.'
 )
 criaCartao(
-    Lingua Portuguesa,
-    pergunta,
-    resposta
+    'Lingua Portuguesa',
+    'O que e o predicado de uma oracao?',
+    'O predicado é tudo aquilo que se declara sobre o sujeito. Ele contém o verbo e as informações sobre a ação ou estado do sujeito. Basicamente, é o resto da oração depois que você tira o sujeito'
 )
 criaCartao(
-    Lingua Portuguesa,
-    pergunta,
-    resposta
+   ' Lingua Portuguesa',
+    'O que faz uma frase virar oracao?',
+    'O que faz uma frase virar oracao e ter um verbo. Sem verbo, e so uma frase'
 )
 criaCartao(
-    Lingua Portuguesa,
-    pergunta,
-    resposta
-)
-criaCartao(
-    Lingua Portuguesa,
-    pergunta,
-    resposta
+    'Lingua Portuguesa',
+    'O que e um verbo?',
+   ' Verbo e a palavra que mostra uma acao (como correr, comer) ou um estado (como ser, estar).'
 )
