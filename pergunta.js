437d923e739a70227cae67c69a0df0a6677385e8
valentrimbeatriz <valentim.beatriz@escola.pr.gov.br>
2025-07-05 18:10:02 -0300

    Update pergunta.js

--- pergunta.js
+++ pergunta.js
@@ -1,50 +1,59 @@
 criaCartao(
     'Lingua Portuguesa',
-    'O que uma oracao sempre precisa ter para ser uma oracao?',
-    Uma oracao sempre precisa ter um verbo
-)
+    'O que uma oração sempre precisa ter para ser uma oração?',
+    'Uma oração sempre precisa ter um verbo'
+);
 
 criaCartao(
-    Lingua Portuguesa,
-    O que indica quem faz ou sofre a ação do verbo em uma oracao,
-    O sujeito
-)
-criaCartao(
-   ' Lingua Portuguesa,'
-    'Qual parte da oracao completa o sentido do verbo, mostrando o que acontece ou o resultado da acao?',
-   ' O predicado'
-)
-criaCartao(
-  '  Lingua Portuguesa',
-    'Como chamamos a oracao que tem apenas um verbo ou locucao verbal?'
-    'Oracao Simples'
-criaCartao(
-   ' Lingua Portuguesa',
-  'Quem e o verbo da frase? Julio comprou banana',
-    'O verbo e comprou' 
-)
+    'Lingua Portuguesa',
+    'O que indica quem faz ou sofre a ação do verbo em uma oração?',
+    'O sujeito'
+);
+
 criaCartao(
     'Lingua Portuguesa',
-     'Se uma oração tem mais de um verbo ou locução verbal, como a chamamos?'
-     'Oração composta.'
-)
-criaCartao(
-   ' Lingua Portuguesa',
-   ' Quem é o sujeito da frase? "Maria comprou um remédio para sua irmã"'
-     'O sujeito é Maria.'
-)
+    'Qual parte da oração completa o sentido do verbo, mostrando o que acontece ou o resultado da ação?',
+    'O predicado'
+);
+
 criaCartao(
     'Lingua Portuguesa',
-    'O que e o predicado de uma oracao?',
-    'O predicado é tudo aquilo que se declara sobre o sujeito. Ele contém o verbo e as informações sobre a ação ou estado do sujeito. Basicamente, é o resto da oração depois que você tira o sujeito'
-)
-criaCartao(
-   ' Lingua Portuguesa',
-    'O que faz uma frase virar oracao?',
-    'O que faz uma frase virar oracao e ter um verbo. Sem verbo, e so uma frase'
-)
+    'Como chamamos a oração que tem apenas um verbo ou locução verbal?',
+    'Oração simples'
+);
+
 criaCartao(
     'Lingua Portuguesa',
-    'O que e um verbo?',
-   ' Verbo e a palavra que mostra uma acao (como correr, comer) ou um estado (como ser, estar).'
-)
+    'Quem é o verbo da frase? "Julio comprou banana"',
+    'O verbo é "comprou"'
+);
+
+criaCartao(
+    'Lingua Portuguesa',
+    'Se uma oração tem mais de um verbo ou locução verbal, como a chamamos?',
+    'Oração composta'
+);
+
+criaCartao(
+    'Lingua Portuguesa',
+    'Quem é o sujeito da frase? "Maria comprou um remédio para sua irmã"',
+    'O sujeito é Maria'
+);
+
+criaCartao(
+    'Lingua Portuguesa',
+    'O que é o predicado de uma oração?',
+    'O predicado é tudo aquilo que se declara sobre o sujeito. Ele contém o verbo e as informações sobre a ação ou estado do sujeito. Basicamente, é o resto da oração depois que você tira o sujeito.'
+);
+
+criaCartao(
+    'Lingua Portuguesa',
+    'O que faz uma frase virar oração?',
+    'O que faz uma frase virar oração é ter um verbo. Sem verbo, é só uma frase.'
+);
+
+criaCartao(
+    'Lingua Portuguesa',
+    'O que é um verbo?',
+    'Verbo é a palavra que mostra uma ação (como correr, comer) ou um estado (como ser, estar).'
+);
